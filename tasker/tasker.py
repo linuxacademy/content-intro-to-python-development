43d84f0ff046afa9898badbfb9485a50d38104f9
Keith Thompson <keith@linuxacademy.com>
2019-03-19 20:41:22 +0000

    Modify doctests for start of control flow lab

--- tasker/tasker.py
+++ tasker/tasker.py
@@ -8,7 +8,7 @@ def print_todo(todo):
     Example 1: This is a test task
     >>>
     """
-    pass
+    print(f"{todo['name']}: {todo['body']}")
 
 def take_first(todos):
     """
@@ -22,16 +22,22 @@ def take_first(todos):
     {'name': 'Example 1', 'body': 'This is a test task', 'points': '3'}
     >>> todos
     [{'name': 'Task 2', 'body': 'Yet another example task', 'points': '2'}]
+    >>> todos = []
+    >>> take_first(todos)
+    (None, [])
     """
-    return
+    todo = todos.pop(0)
+    return (todo, todos)
 
 def sum_points(todo1, todo2):
     """
     sum_points receives two todo dictionaries and returns sum of their `point` values.
 
     >>> todos = [{'name': 'Example 1', 'body': 'This is a test task', 'points': '3'},
-    ... {'name': 'Task 2', 'body': 'Yet another example task', 'points': '2'}]
-    >>> sum_points(todos[0], todos[1])
-    5
+    ... {'name': 'Task 2', 'body': 'Yet another example task', 'points': '2'},
+    ... {'name': 'Task 3', 'body': 'Third task', 'points': '5'}]
+    >>> sum_points(todos)
+    10
     """
-    return
+    return int(todo1['points']) + int(todo2['points'])
+
