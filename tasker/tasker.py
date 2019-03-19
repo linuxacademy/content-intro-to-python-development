f016b1a8d1f60a00957357d27a5f4ee19259bd6a
Keith Thompson <me@keiththomps.com>
2019-03-19 16:05:15 -0400

    Convert point values to be strings
    We would like to have the `points` be strings so that we have to exercise type conversion to get the `sum_points` tests to pass.

--- tasker/tasker.py
+++ tasker/tasker.py
@@ -3,7 +3,7 @@ def print_todo(todo):
     print_todo takes in a todo dictionary and prints it out
     with by separating the `name` from the `body` using a colon (:).
 
-    >>> todo = {'name': 'Example 1', 'body': 'This is a test task', 'points': 3}
+    >>> todo = {'name': 'Example 1', 'body': 'This is a test task', 'points': '3'}
     >>> print_todo(todo)
     Example 1: This is a test task
     >>>
@@ -15,13 +15,13 @@ def take_first(todos):
     take_first receives a list of todos and removes the first todo
     and returns that todo and the remaining todos in a touple
 
-    >>> todos = [{'name': 'Example 1', 'body': 'This is a test task', 'points': 3},
-    ... {'name': 'Task 2', 'body': 'Yet another example task', 'points': 2}]
+    >>> todos = [{'name': 'Example 1', 'body': 'This is a test task', 'points': '3'},
+    ... {'name': 'Task 2', 'body': 'Yet another example task', 'points': '2'}]
     >>> todo, todos = take_first(todos)
     >>> todo
-    {'name': 'Example 1', 'body': 'This is a test task', 'points': 3}
+    {'name': 'Example 1', 'body': 'This is a test task', 'points': '3'}
     >>> todos
-    [{'name': 'Task 2', 'body': 'Yet another example task', 'points': 2}]
+    [{'name': 'Task 2', 'body': 'Yet another example task', 'points': '2'}]
     """
     return
 
@@ -29,8 +29,8 @@ def sum_points(todo1, todo2):
     """
     sum_points receives two todo dictionaries and returns sum of their `point` values.
 
-    >>> todos = [{'name': 'Example 1', 'body': 'This is a test task', 'points': 3},
-    ... {'name': 'Task 2', 'body': 'Yet another example task', 'points': 2}]
+    >>> todos = [{'name': 'Example 1', 'body': 'This is a test task', 'points': '3'},
+    ... {'name': 'Task 2', 'body': 'Yet another example task', 'points': '2'}]
     >>> sum_points(todos[0], todos[1])
     5
     """
